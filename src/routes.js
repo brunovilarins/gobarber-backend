db429fe7e31dc58d6198b9f4e3df5cf675bccc55
Bruno Vilarins <brunovilarins@gmail.com>
2020-01-29 00:57:37 -0300

    Implements create user

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  try {
-    const user = await User.create({
-      name: 'Bruno Vilarins',
-      email: 'brunovilarins@gmail.com',
-      password_hash: '123456',
-      provider: false,
-    });
-    return res.json(user);
-  } catch (e) {
-    return res.json({ message: e });
-  }
-});
+routes.post('/users', UserController.store);
 
 export default routes;
