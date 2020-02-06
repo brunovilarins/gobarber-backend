6065f149345e62b50ba81a3f353fbc4f7c103183
Bruno Vilarins <brunovilarins@gmail.com>
2020-02-05 23:59:52 -0300

    Implements send email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'bb98a04f15005d',
+    pass: 'f464635efd4367',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
