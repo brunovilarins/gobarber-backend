48aa6492b1eaf4ea93297ebf631b2c8a77d14e20
Bruno Vilarins <brunovilarins@gmail.com>
2020-02-07 22:47:20 -0300

    Implements fields Virtual past and cancelable appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
@@ -49,9 +49,9 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
-    // if (provider_id === req.userId) {
-    //   return res.status(400).json({ error: 'Do not permited appointment.' });
-    // }
+    if (provider_id === req.userId) {
+      return res.status(400).json({ error: 'Do not permited appointment.' });
+    }
 
     const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
@@ -120,11 +120,11 @@ class AppointmentController {
       ],
     });
 
-    // if (appointment.user_id !== req.userId) {
-    //   return res.status(401).json({
-    //     error: 'You do not have permission to cancel this appointment',
-    //   });
-    // }
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'You do not have permission to cancel this appointment',
+      });
+    }
 
     const dateWithSub = subHours(appointment.date, 2);
 
